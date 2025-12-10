29bd27b7c974bfdf7bafc86dd0cb7fd22e10fe0e
StylistMercurial1130 <wrk.krthik@gmail.com>
2025-12-10 17:32:48 +0530

    naive implementation : doc store creation, insert, get, get all

--- src/key_value_store.rs
+++ src/key_value_store.rs
@@ -1,11 +1,31 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, fs, io::Write};
+
+use serde_json::Value;
+use uuid::Uuid;
+
+use crate::json::validation::Validator;
+
 /*
 * loading everything to memory seems like kind of a bad idea, but I shall solve this problem
 * later for now loading the entire file
 */
 pub struct Doc {
-    file_name: &'static String,
+    file_name: String,
     json: serde_json::Value,
+}
+
+impl Doc {
+    pub fn new(json_string: String, object_id: &Uuid) -> Self {
+        let file_name = format!("{}.json", object_id.to_string());
+
+        let mut file = fs::File::create(&file_name).expect("failed to create file");
+        file.write_all(json_string.as_bytes())
+            .expect("failed to write to file");
+
+        let json: Value = serde_json::from_str(&json_string).unwrap();
+
+        return Doc { file_name, json };
+    }
 }
 
 /*
@@ -22,5 +42,31 @@ impl DocumentStore {
             store_name: store_name.to_string(),
             store: HashMap::new(),
         };
+    }
+
+    pub fn insert(&mut self, json_string: String) -> Result<Vec<u8>, String> {
+        match Validator::validate_json(&json_string) {
+            Err(e) => return Err(e.to_string()),
+            Ok(_) => {
+                let object_id = Uuid::new_v4();
+
+                self.store.insert(
+                    object_id.into_bytes().to_vec(),
+                    Doc::new(json_string, &object_id),
+                );
+
+                return Ok(object_id.into_bytes().to_vec());
+            }
+        }
+    }
+
+    pub fn get(self, object_id: Vec<u8>) -> Option<String> {
+        return self.store.get(&object_id).map(|doc| {
+            return doc.json.to_string();
+        });
+    }
+
+    pub fn get_all(self) -> Vec<String> {
+        return self.store.values().map(|v| v.json.to_string()).collect();
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+mod json;
+mod key_value_store;
+
+#[cfg(test)]
+mod tests {
+    use crate::json::validation::Validator;
+
+    #[test]
+    fn validator_test() {
+        let valid_json_string = r#"{"a" : "b"}"#;
+        let invalid_json_string = r#"{"a : "b"}"#;
+
+        let mut res = Validator::validate_json(valid_json_string);
+
+        assert_eq!(false, res.is_err());
+
+        res = Validator::validate_json(invalid_json_string);
+
+        assert_eq!(true, res.is_err());
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
